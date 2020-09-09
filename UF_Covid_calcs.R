8ae41a2e91fc7ebce2500ae592a7e4c1539765c3
Emilio M. Bruna <embruna@ufl.edu>
2020-09-09 18:35:27 -0400

    multiple changes
    import from google sheet a seperate script
    compiles multiple days of scraping
    adding data files

--- UF_Covid_calcs.R
+++ UF_Covid_calcs.R
@@ -1,7 +1,8 @@
-library(googlesheets4)
+
 library(tidyverse)
-library(stringr)
 library(rvest)
+
+
 UF_covid_page <- "https://coronavirus.ufhealth.org/screen-test-protect-2/about-initiative/testing-dashboard"
 UF_covid <- read_html(UF_covid_page)
 UF_covid
@@ -55,95 +56,145 @@ value.2<-c("FS.test.RTC","FS.pos.RTC","FS.percpos.RTC",
 
 # UF_covid_data$date<-Sys.Date()
 # UF_covid_data$time<-format(Sys.time(), "%X")
-UF_covid_data<-bind_cols(scrape_date=Sys.Date(),
+UF_web_data<-bind_cols(scrape_date=Sys.Date(),
                          scrape_time=format(Sys.time(), "%X"),
                          update_date=update_date,
                          cats,
                          value.2=value.2,
                          numbers)
 
-write.csv(UF_covid_data,paste("./daily_scrape/UF_covid_data",Sys.Date(),".csv"))
+# Cobnvert counts/percs from character to numeric from 
+UF_web_data$N<-as.numeric(UF_web_data$N)
 
 
 
-##############################################################################
-# this is the old version to scrape off the google sheet set up by reddit user 
-# might still be useful to fill in missing days
-##############################################################################
-raw.data<-read_sheet(ss="18AylEt8G7JF5LZ9A5QQJ3KjvYfr5ZBfZ8g7jFZ8eZ6A",
-                sheet="Raw data",
-                range = "C4:AH",
-                col_names = TRUE,
-                skip=3)
-
-colnames(raw.data)
-
-raw.data<-raw.data %>% select("update_date"="Updated",
-                        "FS.test.RTC"="Faculty and Staff Tested (Without Symptoms)",
-                        "FS.pos.RTC"="Faculty and Staff Positive (Without Symptoms)",
-                        "FS.posperc.RTC"="Percent Positive...4",
-                        "S.test.RTC"="Students Tested (Without Symptoms)",
-                        "S.pos.RTC"="Students Positive (Without Symptoms)",
-                        "S.posperc.RTC"="Percent Positive...7",
-                        "S.test.SHC"="Students With or Without Symptoms Tested for COVID-19",
-                        "S.pos.SHC"="Students With or Without Symptoms Confirmed as Positive",
-                        "S.posperc.SHC"="Percent Positive...10",
-                        "UF.affil.cases"="Cases Identified and Followed Among UF Affiliates",
-                        "contacts.followed"="Contacts of UF Affiliates Identified and Followed",
-                        "UF.isol"="UF Affiliates or Contacts of UF Affiliates Currently in Isolation or Quarantine",
-                        "UF.affil.back"="UF Affiliates Returned to Campus") %>% 
-  gather(value.2, N, 'FS.test.RTC':'UF.affil.back') %>% 
-  arrange(update_date) 
-a<-levels(as.factor(raw.data$value.2))
-b<-c("Contacts of UF Affiliates Identified and Followed",
-     "Faculty and Staff Positive (Without Symptoms)",
-     "Percent Positive",
-     "Faculty and Staff Tested (Without Symptoms)",
-     "Students Positive (Without Symptoms)",
-     "Students With or Without Symptoms Confirmed as Positive",
-     "Percent Positive",
-     "Percent Positive",
-     "Students Tested (Without Symptoms)",
-     "Students With or Without Symptoms Tested for COVID-19",
-     "UF Affiliates Returned to Campus",
-     "Cases Identified and Followed Among UF Affiliates", 
-     "UF Affiliates or Contacts of UF Affiliates Currently in Isolation or Quarantine")
- ab<-bind_cols(a,b)
-names(ab)<-c("value.2","value")
-ab<-ab %>% 
-  arrange(value.2) %>% 
-  bind_cols(testing_program=c("contact.tracing", 
-                                 rep("RTC",4), 
-                                 rep(c("SHCC","RTC"),2),
-                                 "SHCC",
-                                 rep("contact.tracing",3))) %>% 
-  arrange(testing_program) %>% 
-  bind_cols(group=c(rep("uf.affil",4),
-                                 rep("fac.staff",3), 
-                                 rep("students",6)))
-
-raw.data<-left_join(raw.data,ab)
-raw.data$update_date<-as.Date(raw.data$update_date,format("%Y %m %d"))
-raw.data<-raw.data %>% 
-  filter(update_date!="2020-09-08")
-write.csv(raw.data,paste("./daily_scrape/UF_covid_data_20200818-20200907.csv"))
-
-
-
-########now join the archival data and scraped datasets
-archive<-read_csv("./daily_scrape/UF_covid_data_20200818-20200907.csv") %>% 
-  select(-X1)
-scrape<-
-
-corrected_date<-str_split(scrape$update_date,"/")
+# convert date to date and put in proper format
+corrected_date<-str_split(UF_web_data$update_date,"/")
 corrected_date<-data.frame(matrix(unlist(corrected_date), nrow=length(corrected_date), byrow=T))
-corrected_date<-paste(corrected_date$X3,corrected_date$X1,corrected_date$X2,sep="-0")
-corrected_date<-as.Date(corrected_date)
-scrape$update_date<-corrected_date
-scrape$N<-as.double(scrape$N)
+corrected_date$X3<-2020
+corrected_date$X1<-paste(0,corrected_date$X1,sep="")
+corrected_date$X2<-as.numeric(as.character(corrected_date$X2))
+corrected_date <- corrected_date %>% 
+  mutate(X2=ifelse(X2<10,paste(0,X2,sep=""),X2)) %>% 
+  mutate(correct_date=paste(X3,X1,X2,sep="-"))
+corrected_date<-as.Date(corrected_date$correct_date)
+UF_web_data$update_date<-corrected_date
 
-all.data<-bind_rows(archive,scrape)
+# Save as a CSV
+write.csv(UF_web_data,paste("./data_raw/daily_scrape/UF_covid_data_",Sys.Date(),".csv",sep=""))
 
+
+##############################################################
+######## now join the archival data and scraped datasets
+##############################################################
+
+# First bind together all the daily scrapes into a single df
+# path to folder that holds multiple .csv files
+folder <- "./data_raw/daily_scrape/"      
+scrape <- list.files(folder,   # Identify all csv files in folder
+                       pattern = "*.csv", full.names = TRUE) %>% 
+  lapply(read_csv) %>%                                            # Store all files in list
+  bind_rows %>%                                                     # Combine data sets into one data set 
+  arrange(testing_program,group,value,update_date) %>% 
+  select(-X1)
+# data_all<-as.data.frame(data_all)
+scrape
+
+
+archive<-read_csv("./data_raw/googlesheet_import/UF_covid_data_20200818-20200907.csv") %>% 
+  select(-X1) 
+archive
+
+corrected_date<-str_split(archive$update_date,"/")
+corrected_date<-data.frame(matrix(unlist(corrected_date), nrow=length(corrected_date), byrow=T))
+corrected_date$X3<-2020
+corrected_date$X1<-paste(0,corrected_date$X1,sep="")
+corrected_date$X2<-as.numeric(as.character(corrected_date$X2))
+corrected_date <- corrected_date %>% 
+  mutate(X2=ifelse(X2<10,paste(0,X2,sep=""),X2)) %>% 
+  mutate(correct_date<-paste(X3,X1,X2,sep="-"))
+corrected_date<-as.Date(corrected_date$correct_date)
+archive$update_date<-corrected_date
+
+# archive<-archive %>% filter(update_date<"2020-09-08") # choose whatever date you started scraping
+# write.csv(scrape,paste("./data_raw/daily_scrape/UF_covid_data_2020-09-08.csv",sep=""))
+# archive$update_date<-as.Date(archive$update_date,format("%Y %m %d"))
+# archive$update_date<-as.Date(archive$update_date)
+
+uf.data<-bind_rows(archive,scrape)
+
+uf.data.SHC.s<-uf.data %>% 
+  select(update_date,testing_program,group,value.2,N) %>% 
+  filter(testing_program=="SHCC") %>%
+  spread(value.2,N) %>% 
+  select(-S.percpos.SHC) %>% 
+  arrange(update_date) %>% 
+  rename(Npos=S.pos.SHC,Ntest=S.test.SHC)
+
+
+uf.data.RTC.fs<-uf.data %>% 
+  select(update_date,testing_program,group,value.2,N) %>% 
+  filter(testing_program=="RTC" & group=="fac.staff") %>%
+  spread(value.2,N) %>% 
+  select(-FS.percpos.RTC) %>% 
+  arrange(update_date)%>% 
+  rename(Npos=FS.pos.RTC,Ntest=FS.test.RTC)
+
+
+uf.data.RTC.s<-uf.data %>% 
+  select(update_date,testing_program,group,value.2,N) %>% 
+  filter(testing_program=="RTC" & group=="students") %>%
+  spread(value.2,N) %>% 
+  arrange(update_date) %>% 
+  select(-S.percpos.RTC) %>% 
+  rename(Npos=S.pos.RTC,Ntest=S.test.RTC)
+
+
+uf.data<-bind_rows(uf.data.RTC.s,uf.data.RTC.fs,uf.data.SHC.s) %>%
+  group_by(testing_program,group) %>%
+  mutate(new.tests=(Ntest-lag(Ntest)),
+         new.pos=(Npos-lag(Npos)),
+         daily.pos=new.pos/new.tests*100) %>%
+  mutate(cmtv.perc.pos=Npos/Ntest*100) 
+uf.data<-arrange(uf.data,group,update_date)
+
+
+
+# Daily positivity rate with dashed lines for missing days
+my_data<-uf.data %>%
+  arrange(testing_program,group,update_date) %>% 
+  group_by(testing_program,group) %>% 
+  mutate(keep=ifelse(is.na(daily.pos)&lag(is.na(daily.pos)==TRUE),"del","keep")) %>% 
+  arrange(testing_program,group,update_date) %>% 
+  filter(keep=="keep")
+
+gaps <- my_data %>%
+  arrange(testing_program,group,update_date) %>% 
+  group_by(testing_program,group) %>%
+  filter(is.nan(lead(daily.pos)) & row_number() != n() |
+           is.nan(lag(daily.pos)) & row_number() != 1) %>%
+  mutate(gap.group = cumsum(row_number() %% 2)) 
+ 
+UF.plot_daily<-ggplot(data = my_data, mapping = aes(x = update_date, y = daily.pos,color=paste(testing_program,group,sep=" "))) +
+  geom_line() +
+  geom_line(data = filter(gaps,group=="fac.staff" & testing_program=="RTC"), aes(group = gap.group), linetype = "dashed") +
+  geom_line(data = filter(gaps,group=="students" & testing_program=="RTC"), aes(group = gap.group), linetype = "dashed") +
+  geom_line(data = filter(gaps,group=="students" & testing_program=="SHCC"), aes(group = gap.group), linetype = "dashed") +
+  geom_point() +
+  labs(x="Date", y="Daily Positivity Rate (%)")+
+  labs(title = "Daily Positivity Rate")
+UF.plot_daily<-UF.plot_daily + theme_classic()+
+  theme(legend.title=element_blank(),
+        legend.position = c(0.1, 0.9))
+UF.plot_daily
+
+# Cumulative positivity rate
+UF.plot_cumulative<-ggplot(data=uf.data, aes(x=update_date, y=cmtv.perc.pos, color=paste(testing_program,group,sep=" "))) +
+  geom_line() +
+  geom_point()+
+  labs(x="Date", y="Cumulative Positivity Rate (%)")
+UF.plot_cumulative<-UF.plot_cumulative + theme_classic()
+UF.plot_cumulative
 
 
 
@@ -262,17 +313,17 @@ gaps <- my_data %>%
   #  I expect it will break if a point ever has NA's on both sides...
   #  Anyone have a better idea?
   mutate(gap.group = cumsum(row_number() %% 2)) 
-
-UF.plot_daily<-ggplot(data = my_data, mapping = aes(x = date, y = daily.pos,color=group)) +
-  geom_line() +
-  geom_line(data = filter(gaps,group=="SHCC (with or without symptoms) students"), aes(group = gap.group), linetype = "dashed") +
-  geom_line(data = filter(gaps,group=="RTC (without symptoms) Students"), aes(group = gap.group), linetype = "dashed") +
-  geom_line(data = filter(gaps,group=="RTC (without symptoms) Faculty & Staff"), aes(group = gap.group), linetype = "dashed") +
-  geom_point() +
-  labs(x="Date", y="Daily Positivity Rate (%)")+
-  labs(title = "Daily Positivity Rate")
-UF.plot_daily<-UF.plot_daily + theme_classic()
-UF.plot_daily
+# 
+# UF.plot_daily<-ggplot(data = my_data, mapping = aes(x = date, y = daily.pos,color=group)) +
+#   geom_line() +
+#   geom_line(data = filter(gaps,group=="SHCC (with or without symptoms) students"), aes(group = gap.group), linetype = "dashed") +
+#   geom_line(data = filter(gaps,group=="RTC (without symptoms) Students"), aes(group = gap.group), linetype = "dashed") +
+#   geom_line(data = filter(gaps,group=="RTC (without symptoms) Faculty & Staff"), aes(group = gap.group), linetype = "dashed") +
+#   geom_point() +
+#   labs(x="Date", y="Daily Positivity Rate (%)")+
+#   labs(title = "Daily Positivity Rate")
+# UF.plot_daily<-UF.plot_daily + theme_classic()
+# UF.plot_daily
 
 # Cumulative positivity rate
 UF.plot_cumulative<-ggplot(data=UF_data, aes(x=date, y=running.perc.pos, color=group)) +
